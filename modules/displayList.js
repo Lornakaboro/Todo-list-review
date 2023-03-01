ea78ae807699113052a7ac8bc3d27fb318b34a35
Lornakaboro <lornakaboro@gmail.com>
2023-03-01 12:33:25 +0300

    Refctor to use arrow functions

--- modules/displayList.js
+++ modules/displayList.js
@@ -1,69 +1,69 @@
 export default class TaskList {
   constructor() {
+    this.getTasksFromLocalStorage = () => {
+      this.listArray = JSON.parse(localStorage.getItem('taskList')) || [];
+    };
+
+    this.updateTasksInLocalStorage = () => {
+      localStorage.setItem('taskList', JSON.stringify(this.listArray));
+    };
+
     this.getTasksFromLocalStorage();
   }
 
-  updateTasksInLocalStorage() {
-    localStorage.setItem('taskList', JSON.stringify(this.listArray));
-  }
+  getTasks = () => JSON.parse(localStorage.getItem('taskList')) || [];
 
-  getTasksFromLocalStorage() {
-    this.listArray = JSON.parse(localStorage.getItem('taskList')) || [];
-  }
+  setEditTask = (index) => {
+    const task = this.listArray.find(
+      (item) => parseInt(item.index, 10) === parseInt(index, 10),
+    );
+    task.edit = true;
+    this.updateTasksInLocalStorage();
+  };
 
-      getTasks = () => JSON.parse(localStorage.getItem('taskList')) || [];
+  addNewTask = (description) => {
+    const task = {
+      description,
+      completed: false,
+      index: this.listArray.length + 1,
+      edit: false,
+    };
+    this.listArray = [...this.listArray, task];
+    this.updateTasksInLocalStorage();
+  };
 
-      setEditTask(index) {
-        const task = this.listArray.find(
-          (item) => parseInt(item.index, 10) === parseInt(index, 10),
-        );
-        task.edit = true;
-        this.updateTasksInLocalStorage();
-      }
+  removeCompletedTasks = () => {
+    this.listArray = this.listArray.filter((item) => item.completed !== true);
+    if (this.listArray.length > 0) {
+      this.listArray = this.listArray.map((task, i) => {
+        task.index = i + 1;
+        return task;
+      });
+    }
+    this.updateTasksInLocalStorage();
+  };
 
-      addNewTask(description) {
-        const task = {
-          description,
-          completed: false,
-          index: this.listArray.length + 1,
-          edit: false,
-        };
-        this.listArray = [...this.listArray, task];
-        this.updateTasksInLocalStorage();
-      }
+  removeTask = (index) => {
+    this.listArray = this.listArray.filter((item) => item.index !== index);
+    this.listArray = this.listArray.map((task, i) => {
+      task.index = i + 1;
+      return task;
+    });
+    this.updateTasksInLocalStorage();
+  };
 
-      removeCompletedTasks() {
-        this.listArray = this.listArray.filter((item) => item.completed !== true);
-        if (this.listArray.length > 0) {
-          this.listArray = this.listArray.map((task, i) => {
-            task.index = i + 1;
-            return task;
-          });
-        }
-        this.updateTasksInLocalStorage();
-      }
+  editTask = (index, description) => {
+    this.listArray[index - 1].description = description;
+    this.listArray[index - 1].edit = false;
+    this.updateTasksInLocalStorage();
+  };
 
-      removeTask(index) {
-        this.listArray = this.listArray.filter((item) => item.index !== index);
-        this.listArray = this.listArray.map((task, i) => {
-          task.index = i + 1;
-          return task;
-        });
-        this.updateTasksInLocalStorage();
-      }
-
-      editTask(index, description) {
-        this.listArray[index - 1].description = description;
-        this.listArray[index - 1].edit = false;
-        this.updateTasksInLocalStorage();
-      }
-
-      changeTaskCompletionStatus(index) {
-        const status = this.listArray[index - 1].completed;
-        this.listArray[index - 1] = {
-          ...this.listArray[index - 1],
-          completed: !status,
-        };
-        this.updateTasksInLocalStorage();
-      }
+  changeTaskCompletionStatus = (index) => {
+    const status = this.listArray[index - 1].completed;
+    this.listArray[index - 1] = {
+      ...this.listArray[index - 1],
+      completed: !status,
+    };
+    this.updateTasksInLocalStorage();
+  };
 }
